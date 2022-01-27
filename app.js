812e3a07c1e4f1686da6c0e622c0bdf5675b6386
davidajoyce <32209128+davidajoyce@users.noreply.github.com>
2022-01-28 10:50:21 +1100

    feat: add modal for slack channel selection and handle submission

--- app.js
+++ app.js
@@ -61,10 +61,23 @@ app.event('app_home_opened', async ({ event, client, context }) => {
                 "type": "button",
                 "text": {
                   "type": "plain_text",
-                  "text": "Choose Google Drive Folder"
+                  "text": "Choose Google Drive Video File"
                 },
                 "action_id": "first_button",
-                "url": `https://2705-124-150-93-21.ngrok.io/google-driver-picker`
+                "url": `https://db91-124-150-93-21.ngrok.io/google-driver-picker`
+              }
+            ]
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Choose Slack Channel"
+                },
+                "action_id": "second_button"
               }
             ]
           }
@@ -95,4 +108,112 @@ app.action('first_button', async({action, ack, context}) => {
   // acknowledge the request right away
   await ack();
   //await say('Thanks for clicking the fancy button');
+});
+
+// Listen and respond to button click
+app.action('second_button', async({action, client, body, ack, context}) => {
+  console.log('button clicked hi there second button');
+  console.log(action);
+  console.log(body.trigger_id);
+  // acknowledge the request right away
+  await ack();
+
+  try {
+    // Call the views.open method using the WebClient passed to listeners
+    const result = await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        "type": "modal",
+        "callback_id": "slack_channel_modal",
+        "title": {
+          "type": "plain_text",
+          "text": "SLack Channel"
+        },
+        "submit": {
+          "type": "plain_text",
+          "text": "Submit"
+        },
+        "blocks": [
+          {
+            "type": "input",
+            "element": {
+              "type": "plain_text_input",
+              "action_id": "title",
+              "placeholder": {
+                "type": "plain_text",
+                "text": "Please select a slack channel to post your video transcript"
+              }
+            },
+            "label": {
+              "type": "plain_text",
+              "text": "Title"
+            }
+          },
+          {
+            "block_id": "target_channel",
+            "type": "input",
+            "element": {
+              "type": "multi_channels_select",
+              "action_id": "target_select",
+              "placeholder": {
+                "type": "plain_text",
+                "text": "Where should the video transcript be sent?"
+              }
+            },
+            "label": {
+              "type": "plain_text",
+              "text": "Channel(s)"
+            }
+          }
+        ]
+      }
+    });
+
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+
+});
+
+// Handle a view_submission request
+app.view('slack_channel_modal', async ({ ack, body, view, client, logger }) => {
+  // Acknowledge the view_submission request
+  await ack();
+
+  // Do whatever you want with the input data - here we're saving it to a DB then sending the user a verifcation of their submission
+
+  // Assume there's an input block with `block_1` as the block_id and `input_a`
+  const val = view['state']['values']['target_channel']['target_select']['selected_channels'];
+  const user = body['user']['id'];
+  console.log("input from select channel modal")
+  console.log(val);
+  console.log(val.selected_channel);
+
+  /*
+  // Message to send user
+  let msg = '';
+  // Save to DB
+  const results = await db.set(user.input, val);
+
+  if (results) {
+    // DB save was successful
+    msg = 'Your submission was successful';
+  } else {
+    msg = 'There was an error with your submission';
+  }
+
+  // Message the user
+  try {
+    await client.chat.postMessage({
+      channel: user,
+      text: msg
+    });
+  }
+  catch (error) {
+    logger.error(error);
+  }
+  */
+
 });
